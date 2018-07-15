c3c54ecff004b0cb12352c96bc60d7734dfb6526
wanan1001 <852231268@qq.com>
2018-07-15 16:04:46 +0800

    add yaml

--- appender/logback/src/main/java/iloghub/logback/benchmark/Test.java
+++ appender/logback/src/main/java/iloghub/logback/benchmark/Test.java
@@ -1,14 +1,28 @@
 package iloghub.logback.benchmark;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Created by xyc on 2018/7/9.
  */
 public class Test {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Test.class);
+
     public static void main(String[] args) {
-        test();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the configuration file address. If you use the default configuration file, please press Enter.");
+        yamlTest(sc.nextLine());
+        sc.close();
     }
 
     private static void test() {
@@ -19,5 +33,35 @@ public class Test {
             ttList.add(tt);
         }
         ttList.forEach(TestThread::start);
+    }
+
+    private static void yamlTest(String configFileUrl) {
+        try {
+            URL url = getConfigFileUrl(configFileUrl);
+            if (url == null) {
+                System.out.println("config file is not find");
+                return;
+            }
+
+            Yaml yaml = new Yaml();
+            String config = yaml.load(new FileInputStream(url.getFile())).toString();
+            System.out.println(config);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static URL getConfigFileUrl(String configFileUrl) {
+        URL url;
+        try {
+            if (null == configFileUrl || configFileUrl.length() == 0) {
+                url = Test.class.getClassLoader().getResource("config.yaml");
+            } else {
+                url = new URL(configFileUrl);
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return url;
     }
 }
--- appender/logback/src/main/java/iloghub/logback/benchmark/TestThread.java
+++ appender/logback/src/main/java/iloghub/logback/benchmark/TestThread.java
@@ -2,6 +2,7 @@ package iloghub.logback.benchmark;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
 
 /**
  * Created by xyc on 2018/7/9.
@@ -10,11 +11,24 @@ import org.slf4j.LoggerFactory;
 public class TestThread extends Thread {
     private static final Logger LOGGER = LoggerFactory.getLogger(TestThread.class);
 
+    private Integer printLogNum;//0 无限输入 5 每秒发送5条
+
+    public TestThread(Integer printLogNum) {
+        this.printLogNum = printLogNum;
+    }
+
     @Override
     public void run() {
         while (true) {
-            LogTest.printLog(getName());
-            LOGGER.warn(getName() + "print log success" );
+            if (0 == printLogNum) {
+                printLog();
+            } else {
+                
+            }
         }
+    }
+
+    private void printLog() {
+        LOGGER.warn(getName() + "print log test, time is " + System.currentTimeMillis());
     }
 }
