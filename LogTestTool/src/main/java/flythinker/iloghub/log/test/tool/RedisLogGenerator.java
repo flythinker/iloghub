2648bacc5bef1f785f038ed87e654e068bed15e1
flythinker <yipenghuang@hotmail.com>
2018-10-19 14:15:43 +0800

    创建 LogTestTool

---
+++ LogTestTool/src/main/java/flythinker/iloghub/log/test/tool/RedisLogGenerator.java
@@ -0,0 +1,8 @@
+package flythinker.iloghub.log.test.tool;
+
+/**
+ * Created by wlhua on 2018/10/19.
+ */
+public class RedisLogGenerator
+{
+}
---
+++ LogTestTool/src/test/java/flythinker/iloghub/log/test/tool/RedisLogGeneratorDevRun.java
@@ -0,0 +1,20 @@
+package flythinker.iloghub.log.test.tool;
+
+/**
+ * Created by wlhua on 2018/10/19.
+ */
+public class RedisLogGeneratorDevRun {
+    static void test1() throws Exception
+    {
+
+    }
+    public static void main(String[] args){
+
+        try {
+            test1();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
